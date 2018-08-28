367c6a22d69a6ef0bbae1c22dd9f693a93813b56
Jennifer-Hanson <jennkhanson@gmail.com>
2018-08-28 00:06:58 -0400

    added missing map to restaurant page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<pk.eyJ1IjoiamVubmtoYW5zb24iLCJhIjoiY2prdDJtbTdpMDI3MzNrbnh0bXFtczMzcCJ9.IbzbseHBcIloC2QvjHkE5A',
+        mapboxToken: 'pk.eyJ1IjoiamVubmtoYW5zb24iLCJhIjoiY2prdDJtbTdpMDI3MzNrbnh0bXFtczMzcCJ9.IbzbseHBcIloC2QvjHkE5A',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
