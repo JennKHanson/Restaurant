7721e057f5a245a9c5fe92ec908eee1ad88cfd3b
Jennifer-Hanson <jennkhanson@gmail.com>
2018-09-05 23:10:38 -0400

    save

--- service-worker.js
+++ service-worker.js
@@ -33,49 +33,6 @@ const urlsCache = [
  //https://www.youtube.com/watch?v=BfL3pprhnms
 
 //listener for install event
-self.addEventListener('install', function(event){
- console.log("Service Worker Installed")
- //promise -- install after urls are cached
- event.waitUntil(
-   //open cache
-   caches.open(cacheName).then(function(cache) {
-      console.log("Service Worker Caching urlsCache");
-    //fetches urls and adds them to cache
-      return cache.addAll(urlsCache);
-    })
- )
-})
-
-//listener for activate event
-self.addEventListener('activate', function(event){
-  console.log("Service Worker Activated")
-
-  event.waitUntil(
-    //promise -- wait until -- loop through cache names, remove items that don't match
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(cacheNames.map(function(thisCacheName){
-
-          if (thisCacheName !== cacheName) {
-              console.log("Removing cached files from ", thisCacheName);
-              return caches.delete(thisCacheName);
-          }
-
-      }))
-    })
-  )
-})
-
-//listener for fetch event
- self.addEventListener('fetch', function(event){
-   console.log("Service Worker Fetching", event.request.url);
-    //responding with entry from cache, if it exists
-    event.respondWith(
-     caches.match(event.request).then(function(response) {
-        if (response) {
-          console.log("Service Worker found in cache");
-          return response;
-        }
-
-      })
-   )
- })
+self.addEventListener('install', function(e) {
+  //perform install steps
+});
